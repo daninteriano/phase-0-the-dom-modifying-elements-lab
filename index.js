888817ebccaf1bdfe008ea4e15fedad96eeb2bab
Daniel Interiano <dannyboyint@gmail.com>
2021-07-12 16:08:05 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const mainID = document.querySelector('main').remove();
+
+const newHeader = document.querySelector('h1');
+
+const header = document.getElementById('victory');
+header.innerHTML = "Daniel is the champion"
